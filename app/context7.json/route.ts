8c8671d657aff6b87689181b7255fbfb480dae8f
Fallah Andy Prakasa <me@flla.my.id>
2026-07-17 14:50:55 +0700

    feat(docs): serve context7.json manifest for llms.txt indexing

---
+++ app/context7.json/route.ts
@@ -0,0 +1,10 @@
+export const dynamic = "force-static";
+
+export const GET = () => {
+  const body = {
+    public_key: "pk_DjIK3LlmiqANz1Hz6ZTZp",
+    url: "https://context7.com/llmstxt/lilt-ui_vercel_app_llms_txt",
+  };
+
+  return Response.json(body);
+};
